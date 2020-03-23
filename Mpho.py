837e9c2b625ef0c1ee90889c39853fb018d70e41
Mphumutseng <59651577+Mphumutseng@users.noreply.github.com>
2020-03-23 08:21:12 -0700

    Add files via upload

---
+++ Mpho.py
@@ -0,0 +1,33 @@
+print("Hello World")
+activity = "Python tutorial"
+print("Today I will be doing a " + activity + " for beginners")
+print("My end goal for today is to know the basics of python")
+
+phrase = "Python for Beginners"
+print(phrase + " tutorial")
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0], phrase[11])
+print(phrase.replace("Tutorial" ,"Lesson"))
+
+print("Python Numbers")
+from math import*
+print(4* (5 + 5))
+my_num = 10
+print(my_num % 3)
+print(str(my_num) + " tutorial videos to watch")
+print(pow(3, 2))
+print(sqrt(44))
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,4 @@
+name = input("Insert your Name:")
+age = input("Insert your age:")
+print("Hello" + name + "!")
+print("Hello" + name + "!" + " Your are" + age + " years old!" )
